4cad18668b41e580f85ed7a96c19ba4b6dcfae5b
Moises-salas-R <MOISESSAL4S727@GMAIL.COM>
2025-08-27 20:51:02 -0500

    correcion service

--- src/test/java/com/proaula/BankApp/MoisesTests.java
+++ src/test/java/com/proaula/BankApp/MoisesTests.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-
+//moises salas reinel
 public class MoisesTests {
 
     private UsuarioRepository usuarioRepository;
